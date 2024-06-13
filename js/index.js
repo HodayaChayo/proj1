d0548e99d1eff3bec93a2dc4cdfc73286c600914
HodayaChayo <hodayachayo@gmail.com>
2024-06-14 01:21:37 +0300

    hamburger nav

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 'use strict';
 
+// header images slider:
+
 var imgs = document.querySelectorAll('.slideshow > img');
 var dots = document.querySelectorAll('.dot');
 var currentImg = 0;
 const interval = 3000;
 
 function changeSlide(n) {
-  for (var i = 0; i < imgs.length; i++) { // reset
+  for (var i = 0; i < imgs.length; i++) {
+    // reset
     imgs[i].style.opacity = 0;
     dots[i].className = dots[i].className.replace(' active', '');
   }
@@ -14,9 +17,9 @@ function changeSlide(n) {
   currentImg = (currentImg + 1) % imgs.length; // update the index number
 
   if (n != undefined) {
-      clearInterval(timer);
-      timer = setInterval(changeSlide, interval);
-      currentImg = n;
+    clearInterval(timer);
+    timer = setInterval(changeSlide, interval);
+    currentImg = n;
   }
 
   imgs[currentImg].style.opacity = 1;
@@ -24,3 +27,18 @@ function changeSlide(n) {
 }
 
 var timer = setInterval(changeSlide, interval);
+
+// hamburger nav:
+
+const hamburger = document.querySelector('.hamburger');
+const navUl = document.querySelector('.nav-ul');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navUl.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-ul').forEach(link => link.addEventListener('click', ()=>{
+  hamburger.classList.remove('active');
+  navUl.classList.remove('active');
+}))
